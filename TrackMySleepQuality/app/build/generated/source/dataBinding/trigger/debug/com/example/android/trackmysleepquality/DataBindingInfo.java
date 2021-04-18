8b91ecd410583720a55f859dde33c92dfd1eafc9
Kaan Enes KAPICI <kaaneneskpc1@gmail.com>
2021-04-18 23:08:12 +0300

    AndroidBootcamp

--- TrackMySleepQuality/app/build/generated/source/dataBinding/trigger/debug/com/example/android/trackmysleepquality/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.example.android.trackmysleepquality;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- TrackMySleepQuality/app/build/generated/source/kapt/debug/com/example/android/trackmysleepquality/BR.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.trackmysleepquality;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int sleepTrackerViewModel = 1;
-}
